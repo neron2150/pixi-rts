f879cb7105a931bb22a08260bedfdc63ffefdbe3
Nikita Abrazhevich <neron2150@gmail.com>
2023-10-15 20:05:39 +0200

    harvester add function

--- src/game/units/units.ts
+++ src/game/units/units.ts
@@ -6,17 +6,21 @@ import Game from "../game";
 export class Units {
   units: Unit[] = [];
   container = new Container();
- 
+
   constructor(size: number[]) {
     this.container.name = "units";
     const placeholder = new Sprite();
     placeholder.width = size[0] * Game.CELL_SIZE;
     placeholder.height = size[1] * Game.CELL_SIZE;
     this.container.addChild(placeholder);
+    this.addHarvester(1, 2);
+    this.addHarvester(3, 4);
+  }
+  addHarvester(x: number, y: number) {
     this.add(
       new Unit({
-        x: 5,
-        y: 2,
+        x,
+        y,
         width: Game.CELL_SIZE * 2,
         height: Game.CELL_SIZE * 2,
         animatedTexture: Resources.spritesheet.harvester.animations.walk,
